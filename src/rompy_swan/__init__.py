cc695205f6ed6ef121a4417030e95ca3c8141216
rafa-guedes <ocerafa@gmail.com>
2026-04-15 15:55:28 +1200

    Move version below docstrings

--- src/rompy_swan/__init__.py
+++ src/rompy_swan/__init__.py
@@ -1,10 +1,13 @@
-__version__ = "0.9.0"
 """
 SWAN Module for ROMPY
 
 This module provides interfaces and utilities for working with the SWAN
 (Simulating WAves Nearshore) model within the ROMPY framework.
 """
+
+__version__ = "0.9.0"
+
+
 from rompy.logging import LoggingConfig, get_logger
 
 logger = get_logger(__name__)
